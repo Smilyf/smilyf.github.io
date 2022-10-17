6cd8a0a2cd1fc654ce55f5239b1b50592067a7b2
smilyf <yam_lady@petalmail.com>
2022-10-17 12:29:42 +0800

    highlight

--- js/index1.js
+++ js/index1.js
@@ -4,8 +4,8 @@
 
 window.addEventListener('load', () => {
 
+    
     var artshow = document.querySelector(".content-show")
-    // banner.style.display = "none"
     artshow.style.display = "inline-block"
     let url = "../article/" + localStorage.getItem('item') + ".md"
     fetch(url)
@@ -13,10 +13,12 @@ window.addEventListener('load', () => {
             data.text().then((text) => {
                 let temp = document.createElement("div")
                 temp.innerHTML = marked.parse(text)
-                hljs.highlightBlock(temp)
-                artshow.appendChild(temp)
+                temp.querySelectorAll("pre code").forEach((el) => {
+                    hljs.highlightElement(el);
+                  });
                 
-               
+                
+                artshow.appendChild(temp)
             })
              
         })
