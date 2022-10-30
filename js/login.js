3b7974a3c24f05db7944acf22d66e94e363a2d70
smilyf <yam_lady@petalmail.com>
2022-10-30 08:37:29 +0800

    5

---
+++ js/login.js
@@ -0,0 +1,8 @@
+// window.addEventListener("load",()=>{
+//     document.querySelector("#register").addEventListener("click",()=>{
+//         // const title = ''
+//         // const url = 'hello-world.html'    
+//         alert("dsa")
+//     })
+   
+// })
--- js/user.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load",()=>{
-    document.querySelector("#register").addEventListener("click",()=>{
-        // const title = ''
-        // const url = 'hello-world.html'    
-        alert("dsa")
-    })
-   
-})
