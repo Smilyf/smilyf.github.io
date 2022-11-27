f92a7ae472ef9d9f013028d33c4371f197371fef
smilyf <yam_lady@petalmail.com>
2022-11-27 17:59:23 +0800

    das

--- js/individualSpace.js
+++ js/individualSpace.js
@@ -1,4 +1,5 @@
 "use strict";
+
 import hljs from 'https://unpkg.com/@highlightjs/cdn-assets@11.6.0/es/highlight.min.js';
 //  and it's easy to individually load & register additional languages
 import go from 'https://unpkg.com/@highlightjs/cdn-assets@11.6.0/es/languages/go.min.js';
@@ -178,7 +179,6 @@ function select(content_index) {
 }
 
 
-
 window.addEventListener('load', () => {
     article_display()
 
@@ -192,9 +192,6 @@ window.addEventListener('load', () => {
         const synopsis = document.querySelector("#synopsis")
 
 
-
-
-
     })
     document.querySelector("#manuscript").addEventListener("click", () => {
         select("manuscript")
@@ -204,10 +201,196 @@ window.addEventListener('load', () => {
 })
 window.addEventListener("load", () => {
 
+    let full_preview = document.querySelector("#full_preview")
+    let full_un_preview = document.querySelector("#full_un_preview")
+    let un_full_preview = document.querySelector("#un_full_preview")
+    let un_full_un_preview=document.querySelector("#un_full_un_preview")
+    let full_screen_div_left = document.querySelector("#full_screen_div_left")
+    let full_screen_div_right = document.querySelector("#full_screen_div_right")
+    let un_full_screen_div_left = document.querySelector("#un_full_screen_div_left")
+    let un_full_screen_div_right = document.querySelector("#un_full_screen_div_right")
+
+    full_preview.addEventListener("click", () => {
+
+        full_preview.className = "div_svg_hidden"
+        full_un_preview.className = "div_svg_show"
+        full_screen_div_left.className = "content-left-unfull"
+        full_screen_div_right.className = "content-right-full"
+
+    })
+    full_un_preview.addEventListener("click", () => {
+
+        full_un_preview.className = "div_svg_hidden"
+        full_preview.className = "div_svg_show"
+        full_screen_div_left.className = "content-left-full"
+        full_screen_div_right.className = "content-right-unfull"
+
+    })
+
+    un_full_un_preview.addEventListener("click", () => {
+
+
+        un_full_un_preview.className = "div_svg_hidden"
+        un_full_preview.className = "div_svg_show"
+        un_full_screen_div_left.className = "content-left-full"
+        un_full_screen_div_right.className = "content-right-unfull"
+    })
+
+    un_full_preview.addEventListener("click", () => {
+
+        un_full_un_preview.className = "div_svg_show"
+        un_full_preview.className = "div_svg_hidden"
+        un_full_screen_div_left.className = "content-left-unfull"
+        un_full_screen_div_right.className = "content-right-full"
+
+    })
+
+})
+
+
+
+
+
+window.addEventListener("load", () => {
+
+    let text;
+    let full_screen_content_right = document.querySelector("#full_screen_content_right")
+    let un_full_screen_content_right = document.querySelector("#un_full_screen_content_right")
+    let full_screen_content_left = document.querySelector("#full_screen_content_left")
+    let un_full_screen_content_left = document.querySelector("#un_full_screen_content_left")
+
+    if (sessionStorage.getItem("content") != null) {
+        let temp = document.createElement("div")
+        let temp1 = document.createElement("div")
+
+        full_screen_content_right.innerHTML = ""
+        un_full_screen_content_right.innerHTML = ""
+        text = sessionStorage.getItem("content")
+        un_full_screen_content_left.value = text
+        full_screen_content_left.value = text
+        sessionStorage.setItem("content", text)
+
+        temp.innerHTML = marked.parse(text)
+
+        for (const element of temp.querySelectorAll("pre code")) {
+            hljs.highlightElement(element);
+        }
+
+        temp1.innerHTML = marked.parse(text)
+
+        for (const element of temp1.querySelectorAll("pre code")) {
+            hljs.highlightElement(element);
+        }
+
+        full_screen_content_right.appendChild(temp)
+
+        un_full_screen_content_right.appendChild(temp1)
+
+    }
+    full_screen_content_left.addEventListener('input', () => {
+
+
+        let temp = document.createElement("div")
+        let temp1 = document.createElement("div")
+
+        full_screen_content_right.innerHTML = ""
+        un_full_screen_content_right.innerHTML = ""
+        text = full_screen_content_left.value;
+        un_full_screen_content_left.value = full_screen_content_left.value;
+        sessionStorage.setItem("content", text)
+
+        temp.innerHTML = marked.parse(text)
+
+        for (const element of temp.querySelectorAll("pre code")) {
+            hljs.highlightElement(element);
+        }
+
+        temp1.innerHTML = marked.parse(text)
+
+        for (const element of temp1.querySelectorAll("pre code")) {
+            hljs.highlightElement(element);
+        }
+
+        full_screen_content_right.appendChild(temp)
+
+        un_full_screen_content_right.appendChild(temp1)
+
+
+
+    })
+
+    un_full_screen_content_left.addEventListener('input', () => {
+
+
+        let temp = document.createElement("div")
+        let temp1 = document.createElement("div")
+
+        full_screen_content_right.innerHTML = ""
+        un_full_screen_content_right.innerHTML = ""
+        text = un_full_screen_content_left.value;
+        full_screen_content_left.value = un_full_screen_content_left.value;
+        sessionStorage.setItem("content", text)
+
+        temp.innerHTML = marked.parse(text)
+
+        for (const element of temp.querySelectorAll("pre code")) {
+            hljs.highlightElement(element);
+        }
+
+        temp1.innerHTML = marked.parse(text)
+
+        for (const element of temp1.querySelectorAll("pre code")) {
+            hljs.highlightElement(element);
+        }
+
+        full_screen_content_right.appendChild(temp)
+
+        un_full_screen_content_right.appendChild(temp1)
+
+    })
+})
+
+
+
+
+window.addEventListener("load", () => {
+
+    let full_screen = document.querySelector("#full_screen")
+    let full_screen_show = document.querySelector("#full_screen_show")
+    let layout = document.querySelector("#layout")
+    let un_full_screen = document.querySelector("#un_full_screen")
+
+    full_screen.addEventListener("click", () => {
+        layout.className = "layout"
+        full_screen_show.className = "full_screen_hidden"
+
+    })
+
+    un_full_screen.addEventListener("click", () => {
+
+        layout.className = "layout_hidden"
+        full_screen_show.className = "full_screen"
+    })
+
+})
+
+
+
+window.addEventListener("load", () => {
+
     if (sessionStorage.getItem("identity") != null) {
         document.querySelector("#identity-user").className = "user"
         document.querySelector("#identity-visitor").className = "user-hidden"
     }
+
+
+})
+
+
+
+
+
+
 
     // document.querySelector("#color").addEventListener("change", (event)=>{
 
@@ -215,30 +398,3 @@ window.addEventListener("load", () => {
 
     // }
     // );
-
-
-
-   
-    let content = document.querySelector("#content")
-    let text;
-    let artshow = document.querySelector(".content-show")
-    artshow.style.display = "inline-block"
-    let temp = document.createElement("div")
-
-    let content_exist=sessionStorage.getItem("content")
-    if(content_exist!=null)
-    {
-        text=content_exist;
-    }
-    content.addEventListener('input', () => {
-        text = content.value;
-        temp.innerHTML = ""
-        temp.innerHTML = marked.parse(text)
-        sessionStorage.setItem("content",text)
-        for (const element of temp.querySelectorAll("pre code")) {
-            hljs.highlightElement(element);
-            // alert("123")
-        }
-        artshow.appendChild(temp)
-    })
-})
