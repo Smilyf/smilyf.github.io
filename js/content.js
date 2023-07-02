ed825aeffbb30d12d2ac9425225e329b1e9d785e
smilyf <yam_lady@petalmail.com>
2023-07-02 13:46:17 +0800

    公式

--- js/content.js
+++ js/content.js
@@ -4,6 +4,54 @@ import go from 'https://unpkg.com/@highlightjs/cdn-assets@11.6.0/es/languages/go
 
 hljs.registerLanguage('go', go);
 
+
+const renderer = new marked.Renderer()
+let originParagraph = renderer.paragraph.bind(renderer)
+renderer.paragraph = (text) => {
+  const blockRegex = /\$\$[^\$]*\$\$/g
+  const inlineRegex = /\$[^\$]*\$/g
+  let blockExprArray = text.match(blockRegex)
+  let inlineExprArray = text.match(inlineRegex)
+  for (let i in blockExprArray) {
+    const expr = blockExprArray[i]
+    const result = renderMathsExpression(expr)
+    text = text.replace(expr, result)
+  }
+  for (let i in inlineExprArray) {
+    const expr = inlineExprArray[i]
+    const result = renderMathsExpression(expr)
+    text = text.replace(expr, result)
+  }
+  return originParagraph(text)
+}
+function renderMathsExpression (expr) {
+  if (expr[0] === '$' && expr[expr.length - 1] === '$') {
+    let displayStyle = false
+    expr = expr.substr(1, expr.length - 2)
+    if (expr[0] === '$' && expr[expr.length - 1] === '$') {
+      displayStyle = true
+      expr = expr.substr(1, expr.length - 2)
+    }
+    let html = null
+    try {
+      html = katex.renderToString(expr)
+    } catch (e) {
+      console.err(e)
+    }
+    if (displayStyle && html) {
+      html = html.replace(/class="katex"/g, 'class="katex katex-block" style="display: block;"')
+    }
+    return html
+  } else {
+    return null
+  }
+}
+marked.setOptions({renderer: renderer})
+
+
+
+
+
 function getBasePath() {
     var obj = window.location;
     var contextPath = obj.pathname.split("/")[0];
